27769ed636cae69ae8662c2474a29dc881b47f3a
PinwheelChoi <pinwheelchoi@gmail.com>
2022-12-21 15:37:19 -0500

    Updated:enable to update random dates

--- src/domain/Advertiser/EditBill.js
+++ src/domain/Advertiser/EditBill.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 // import { format, isAfter, isBefore, isSameDay, parse, parseISO } from 'date-fns';
-import { Button, Grid, Checkbox, TextField, Dialog, DialogTitle, DialogContent, DialogActions, FormGroup, FormControlLabel } from "@mui/material";
+import { addYears, compareAsc, eachMonthOfInterval, endOfMonth, format, isAfter, isBefore, isSameDay, endOfYear, startOfYear } from 'date-fns';
+import { Button, Grid, Checkbox, TextField, Dialog, DialogTitle, DialogContent, DialogActions, FormGroup, FormControlLabel, Chip, Typography } from "@mui/material";
 // import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 // import { styled } from '@mui/material/styles'
+
 import { LoadingButton } from '@mui/lab';
 import ClearIcon from '@mui/icons-material/Clear';
 import SaveIcon from '@mui/icons-material/Save';
@@ -17,6 +19,10 @@ import ADTypeSize from '../ADScheduling/ADTypeSize';
 import useSales from 'shared/hooks/useSales';
 import { useUserAuth } from 'shared/contexts/UserAuthContext';
 
+import { adTypeCode, UI_DATE_FORMAT, DATA_DATE_FORMAT, roleType } from 'data/constants';
+import ADDate from 'shared/components/ADDate';
+import SnackbarMessage from 'shared/components/SnackbarMessage';
+
 const OptionCheck = ({ checked, label, onChange, sx }) => {
   return (
     <FormGroup>
@@ -24,11 +30,12 @@ const OptionCheck = ({ checked, label, onChange, sx }) => {
         control={<Checkbox size="small" checked={checked} onChange={onChange} />}
         labelPlacement="top" sx={{ "& .MuiFormControlLabel-label": { fontSize: 11 }, mx: 0, ...sx }}
       />
-    </FormGroup>    
+    </FormGroup>
   );
 };
 
-const EditBill = ({gridApi, selectedNode: bill, onSaved, onClose}) => {
+const EditBill = ({ gridApi, selectedNode: bill, randomDates, onSaved, onClose }) => {
+  const curDate = new Date();
   // const [bill, setBill] = useState({});
   const [adTitle, setAdTitle] = useState(bill.adTitle);
   const [cadTitle, setCadTitle] = useState(bill.cadTitleCode);
@@ -42,6 +49,29 @@ const EditBill = ({gridApi, selectedNode: bill, onSaved, onClose}) => {
   const [color, setColor] = useState(bill.color);
   const [cardPayment, setCardPayment] = useState(bill.cardPayment);
   const [taxIncluded, setTaxIncluded] = useState(false);
+
+  const [updatedRandomDates, setUpdatedRandomDates] = useState([]);
+  const [stDate, setStDate] = useState(new Date(+bill.startDate.substr(0,4), +bill.startDate.substr(4,2)-1, +bill.startDate.substr(6,2)));
+  const [edDate, setEdDate] = useState(new Date(+bill.endDate.substr(0,4), +bill.endDate.substr(4,2)-1, +bill.endDate.substr(6,2), 23, 59, 59));
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleDeleteDate = (date) => {
+    const newDates = updatedRandomDates.filter(currentDates => currentDates !== date);
+    setUpdatedRandomDates(newDates);
+  }
+
+  const handleRandomDateChange = (selectedDate) => {
+    if (isAfter(selectedDate, edDate) || isBefore(selectedDate, stDate)) {
+      setErrorMessage(`Please select a date between ${format(stDate, UI_DATE_FORMAT)} and ${format(edDate, UI_DATE_FORMAT)}`);
+      return;
+    }
+
+    const fmtDate = format(selectedDate, DATA_DATE_FORMAT);
+    const dupDate = updatedRandomDates.find(saved => saved === fmtDate);
+
+    if (!dupDate) setUpdatedRandomDates([...updatedRandomDates, fmtDate]);
+  };
+
   const { user } = useUserAuth();
 
   const { isLoading, updateAD } = useSales();
@@ -57,7 +87,7 @@ const EditBill = ({gridApi, selectedNode: bill, onSaved, onClose}) => {
 
   const handleTaxIncluded = (e) => {
     const taxIncluded = e.target.checked;
-    
+
     setTaxIncluded(taxIncluded);
     const { cost: newCost, tax: newTax } = calculateTaxWithTaxIncluded(cost, tax, taxIncluded);
 
@@ -66,18 +96,21 @@ const EditBill = ({gridApi, selectedNode: bill, onSaved, onClose}) => {
   };
 
   const saveChanges = async () => {
-    const data = { 
-      adId: bill.adId, 
-      companyId: bill.companyId, 
-      adTitle, 
-      cadTitle: cadTitle === -1 ? "" : cadTitle, 
-      page, 
-      size, 
-      cost: Number(removeCommas(cost)), 
-      taxAmount: tax, 
-      color, 
+    const data = {
+      adId: bill.adId,
+      companyId: bill.companyId,
+      adTitle,
+      cadTitle: cadTitle === -1 ? "" : cadTitle,
+      page,
+      size,
+      cost: Number(removeCommas(cost)),
+      taxAmount: tax,
+      color,
       cardPayment,
-      updatedBy: user.userId };
+      updatedBy: user.userId,
+      scheduleType: bill.scheduleTypeCode,
+      updatedRandomDates
+    };
 
     await updateAD(data);
     onSaved();
@@ -85,24 +118,35 @@ const EditBill = ({gridApi, selectedNode: bill, onSaved, onClose}) => {
 
   useEffect(() => {
     if (cost) setTotal(precisionRound(Number(removeCommas(cost)) + +tax))
-  }, [tax]);
+
+    //check to show random dates
+    if (randomDates?.length > 0) {
+      const dates = randomDates.map(date => date.startDate);
+      setUpdatedRandomDates(dates);
+    }
+    else {
+      setUpdatedRandomDates([]);
+    }
+  }, [tax, randomDates]);
 
   return (
-    <Dialog
-      open={true}
-      onClose={() => onClose()}
-      sx={{ "& .MuiPaper-root": { minWidth: 500, minHeight: 200 }}}
-    >
-      <DialogTitle>Edit Bill - {bill.adId}</DialogTitle>
+    <>
+      <SnackbarMessage errorMessage={errorMessage} onClose={() => setErrorMessage('')} />
+      <Dialog
+        open={true}
+        onClose={() => onClose()}
+        sx={{ "& .MuiPaper-root": { minWidth: 500, minHeight: 200 } }}
+      >
+        <DialogTitle>Edit Bill - {bill.adId}</DialogTitle>
 
-      <DialogContent>
-        <Grid container columnGap={1} wrap="nowrap" sx={{ mb: 4 }}>
-          <Grid item xs={12}>
-            <TextField label="AD Title" variant="standard" fullWidth
-              value={adTitle} onChange={(e) => setAdTitle(e.target.value)}
-            />
-          </Grid>
-          {/* 
+        <DialogContent>
+          <Grid container columnGap={1} wrap="nowrap" sx={{ mb: 4 }}>
+            <Grid item xs={12}>
+              <TextField label="AD Title" variant="standard" fullWidth
+                value={adTitle} onChange={(e) => setAdTitle(e.target.value)}
+              />
+            </Grid>
+            {/* 
           <Grid item>
             <ADDate label="Start Date"
               onChange={(value) => {
@@ -132,58 +176,73 @@ const EditBill = ({gridApi, selectedNode: bill, onSaved, onClose}) => {
             />                  
           </Grid>  
           */}
-        </Grid>
-
-        <Grid container columnGap={3} wrap="nowrap">
-          <Grid item xs={1}>
-            <TextField label="Page" value={page} variant="standard" onChange={e => setPage(e.target.value)} />
           </Grid>
-          <Grid item xs={1}>
-            {/* <TextField label="Size" value={size} 
+
+          <Grid container columnGap={3} wrap="nowrap">
+            <Grid item xs={1}>
+              <TextField label="Page" value={page} variant="standard" onChange={e => setPage(e.target.value)} />
+            </Grid>
+            <Grid item xs={1}>
+              {/* <TextField label="Size" value={size} 
               variant="standard"
             /> */}
-            <ADTypeSize type={bill.adTypeCode} width="100%" value={size} onChange={(e) => setSize(e.target.value)} noNone />            
-          </Grid>
-          {bill.adType === "CAD" && (
-            <Grid item xs={2}>
-              <Dropdown label="CAD Title" options={cadTitles} noNone fullWidth
-                value={cadTitle} onChange={e => setCadTitle(e.target.value)}
-              />
+              <ADTypeSize type={bill.adTypeCode} width="100%" value={size} onChange={(e) => setSize(e.target.value)} noNone />
             </Grid>
+            {bill.adType === "CAD" && (
+              <Grid item xs={2}>
+                <Dropdown label="CAD Title" options={cadTitles} noNone fullWidth
+                  value={cadTitle} onChange={e => setCadTitle(e.target.value)}
+                />
+              </Grid>
+            )}
+            <Grid item xs={2}>
+              <ADPrice label="Cost" value={cost} onChange={e => setCost(e.target.value)} onBlur={handleBlurOnCost} />
+            </Grid>
+            <Grid item xs={2}>
+              <ADPrice label="Tax" value={tax} readOnly />
+            </Grid>
+            <Grid item xs={2}>
+              <ADPrice label="Total" value={total} readOnly />
+            </Grid>
+          </Grid>
+
+          <Grid container wrap="nowrap" sx={{ mt: 4 }}>
+            <Grid item xs={2}>
+              <OptionCheck label="Tax Included" checked={taxIncluded} onChange={handleTaxIncluded} />
+            </Grid>
+            <Grid item xs={2}>
+              <OptionCheck label="Card Pay" checked={cardPayment} onChange={e => setCardPayment(e.target.checked)} />
+            </Grid>
+            <Grid item xs={2}>
+              <OptionCheck label="Color" checked={color} onChange={e => setColor(e.target.checked)} />
+            </Grid>
+            <Grid>
+              {updatedRandomDates.length > 0 && (
+                <ADDate label="Update Dates" closeOnSelect={false} onChange={(newValue) => handleRandomDateChange(newValue)} />
+              )}
+            </Grid>
+          </Grid>
+        </DialogContent>
+
+        <DialogActions>
+          {updatedRandomDates?.length > 0 && (
+            <Grid container alignItems="center" columnGap={1} rowGap={2} sx={{ mt: 4, mb: 4 }}>
+              {updatedRandomDates.sort((a, b) =>
+                compareAsc(new Date(a), new Date(b))).map(date =>
+                  <Chip key={date} onDelete={() => handleDeleteDate(date)} label={date} />
+                )}
+            </Grid>
+
           )}
-          <Grid item xs={2}>
-            <ADPrice label="Cost" value={cost} onChange={e => setCost(e.target.value)} onBlur={handleBlurOnCost} />
-          </Grid>
-          <Grid item xs={2}>
-            <ADPrice label="Tax" value={tax} readOnly />
-          </Grid>
-          <Grid item xs={2}>
-            <ADPrice label="Total" value={total} readOnly />
-          </Grid>  
-        </Grid>
-
-        <Grid container wrap="nowrap" sx={{ mt: 4 }}>
-          <Grid item xs={2}>
-            <OptionCheck label="Tax Included" checked={taxIncluded} onChange={handleTaxIncluded} />
-          </Grid>
-          <Grid item xs={2}>
-            <OptionCheck label="Card Pay" checked={cardPayment} onChange={e => setCardPayment(e.target.checked)} />
-          </Grid>
-          <Grid item xs={2}>
-            <OptionCheck label="Color" checked={color} onChange={e => setColor(e.target.checked)} />
-          </Grid>          
-        </Grid>
-      </DialogContent>
-
-      <DialogActions>
-        <LoadingButton loading={isLoading} startIcon={<SaveIcon />} variant="contained"
-          onClick={saveChanges}
-        >
-          Save
-        </LoadingButton>
-        <Button startIcon={<ClearIcon />} onClick={() => onClose()} variant="outlined">Cancel</Button>
-      </DialogActions>
-    </Dialog>
+          <LoadingButton loading={isLoading} startIcon={<SaveIcon />} variant="contained"
+            onClick={saveChanges}
+          >
+            Save
+          </LoadingButton>
+          <Button startIcon={<ClearIcon />} onClick={() => onClose()} variant="outlined">Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
