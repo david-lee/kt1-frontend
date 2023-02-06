723767b925f74db3cd085baa258bcdd6917764e9
PinwheelChoi <pinwheelchoi@gmail.com>
2023-02-06 12:08:50 -0500

    Update: added a reload button

--- src/domain/Advertiser/MonthlySales.js
+++ src/domain/Advertiser/MonthlySales.js
@@ -1,54 +1,75 @@
 import React, { useEffect, useState } from 'react';
 import api from 'appConfig/restAPIs';
 import axios from 'axios';
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { AgGridReact } from 'ag-grid-react';
 import { numberWithCommas } from 'shared/utils';
+import { LoadingButton } from '@mui/lab';
+import ReplayIcon from '@mui/icons-material/Replay';
 
-const MonthlySales = ({ companyId, tabIndex }) => {
+const MonthlySales = ({ companyId }) => {
     const [sales, setSales] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
     const columnDefs = [
-        { field: 'adType', headerName: 'AD Type', width: 80, resizable: false },
-        { field: 'sum', headerName: 'Sales Amount', width: 120, resizable: false, valueFormatter: params => numberWithCommas(params.data.sum.toFixed(2)) }
+      { field: 'adType', headerName: 'AD Type', width: 80, resizable: false },
+      { field: 'sum', headerName: 'Sales Amount', width: 120, resizable: false, valueFormatter: params => numberWithCommas(params.data.sum.toFixed(2)) }
     ];
 
     useEffect(() => {
-        setIsLoading(true);
-        axios.get(`${api.monthlySales}/${companyId}`)
-            .then(({ data }) => {
-                const total = data.reduce((pre, cur) => { return pre + cur.sum}, 0);
-                setSales([{adType: 'TOTAL', sum: total}, ...data]);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }, [companyId, tabIndex===4])
+      setIsLoading(true);
+      fetchMonthlySales();
+    }, [companyId])
+
+    const fetchMonthlySales = () => {
+      axios.get(`${api.monthlySales}/${companyId}`)
+        .then(({ data }) => {
+          const total = data.reduce((pre, cur) => { return pre + cur.sum }, 0);
+            setSales([{ adType: 'TOTAL', sum: total }, ...data]);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
 
     if (isLoading) return <div>loading...</div>
 
-    if (!sales) return  null;
+    if (!sales) return null;
 
     return (
-        <Box>
+      <Grid container direction="column" rowGap={3}>
+        <Grid item>
+          <LoadingButton loading={isLoading}
+            variant="contained"
+              onClick={() => {
+                fetchMonthlySales();
+              }}
+              startIcon={<ReplayIcon />}
+          >
+            Reload
+          </LoadingButton>
+        </Grid>
+        <Grid item>
+          <Box>
             <Box sx={{ width: 240, height: 200 }} className="ag-theme-alpine">
-                <AgGridReact
-                    rowData={sales}
-                    columnDefs={columnDefs}
-                    defaultColDef={{
-                        sortable: true,
-                        resizable: true,
-                        cellStyle: () => ({
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center"
-                          })
-                    }}
-                >
-                </AgGridReact>
+              <AgGridReact
+                rowData={sales}
+                columnDefs={columnDefs}
+                defaultColDef={{
+                  sortable: true,
+                  resizable: true,
+                  cellStyle: () => ({
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                  })
+                }}
+              >
+              </AgGridReact>
             </Box>
-        </Box>
+          </Box>
+        </Grid>
+      </Grid>
     )
 }
 
