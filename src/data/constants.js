a3c2b42208314a5903974647b5a0c4c82be534d9
PinwheelChoi <pinwheelchoi@gmail.com>
2024-02-28 11:24:47 -0500

    update info

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,12 +5,12 @@ export const AD_TAX = 0.13;
 export const UI_DATE_FORMAT = 'yyyy/MM/dd';
 export const DATA_DATE_FORMAT = 'yyyyMMdd';
 export const DEFAULT_ADDRESS = { addr: '-', city: '-', province: '-', postal: '-' };
-// export const API_BASE_URL = 'https://api.koreatimes.net/api/v1';
-export const API_BASE_URL = 'https://api.dev.koreatimes.net/api/v1';
-//  export const API_BASE_URL = 'https://localhost:5000/api/v1';
+export const API_BASE_URL = 'https://api.koreatimes.net/api/v1';
+// export const API_BASE_URL = 'https://api.dev.koreatimes.net/api/v1';
+// export const API_BASE_URL = 'https://localhost:5000/api/v1';
 
-export const STRIPE_PUB_KEY = "pk_test_51J0pcLFTcVYEN1aoOOqeqOnEaJIW0lKq0K3D2m0X6NSemN8D9oIeKgJ740KYoGdQMOFzbhWsxSoaNbZIBEh9qmfx00fbhXsT3Q";
-// export const STRIPE_PUB_KEY = "pk_live_51J0pcLFTcVYEN1aoFspMOmRHY0X5FO5EhJro9x1xVkarTL3wvfIz2dDdrJ4U2sL362IhltlTySVYGQBrExCysSrT00JcdmqTHm";
+export const STRIPE_PUB_KEY = "pk_live_51J0pcLFTcVYEN1aoFspMOmRHY0X5FO5EhJro9x1xVkarTL3wvfIz2dDdrJ4U2sL362IhltlTySVYGQBrExCysSrT00JcdmqTHm";
+// export const STRIPE_PUB_KEY = "pk_test_51J0pcLFTcVYEN1aoOOqeqOnEaJIW0lKq0K3D2m0X6NSemN8D9oIeKgJ740KYoGdQMOFzbhWsxSoaNbZIBEh9qmfx00fbhXsT3Q";
 
 export const codeType = {
   userType: 1,
