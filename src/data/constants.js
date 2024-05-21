11cc06c3dbbb6b6464efa96c9f066bffecd394a2
Sion Lee <35111239+sionara@users.noreply.github.com>
2024-05-20 21:08:39 -0400

    added auto-fill button to BillList component

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,12 +4,18 @@ export const RECURRING_ONE_TIME = '3';
 export const AD_TAX = 0.13;
 export const UI_DATE_FORMAT = 'yyyy/MM/dd';
 export const DATA_DATE_FORMAT = 'yyyyMMdd';
-export const DEFAULT_ADDRESS = { addr: '-', city: '-', province: '-', postal: '-' };
+export const DEFAULT_ADDRESS = {
+  addr: '-',
+  city: '-',
+  province: '-',
+  postal: '-',
+};
 export const API_BASE_URL = 'https://api.koreatimes.net/api/v1';
 // export const API_BASE_URL = 'https://api.dev.koreatimes.net/api/v1';
 // export const API_BASE_URL = 'https://localhost:5000/api/v1';
 
-export const STRIPE_PUB_KEY = "pk_live_51J0pcLFTcVYEN1aoFspMOmRHY0X5FO5EhJro9x1xVkarTL3wvfIz2dDdrJ4U2sL362IhltlTySVYGQBrExCysSrT00JcdmqTHm";
+export const STRIPE_PUB_KEY =
+  'pk_live_51J0pcLFTcVYEN1aoFspMOmRHY0X5FO5EhJro9x1xVkarTL3wvfIz2dDdrJ4U2sL362IhltlTySVYGQBrExCysSrT00JcdmqTHm';
 // export const STRIPE_PUB_KEY = "pk_test_51J0pcLFTcVYEN1aoOOqeqOnEaJIW0lKq0K3D2m0X6NSemN8D9oIeKgJ740KYoGdQMOFzbhWsxSoaNbZIBEh9qmfx00fbhXsT3Q";
 
 export const codeType = {
@@ -23,7 +29,7 @@ export const codeType = {
   status: 8,
   adType: 9,
   page: 10,
-  size: 11, 
+  size: 11,
   title: 12,
   taxRate: 13,
   payMethod: 14,
@@ -33,15 +39,15 @@ export const codeType = {
 export const companyStatus = {
   confirmed: 1908,
   deleted: 1910,
-}
+};
 
 export const adTypeCode = {
   dad: '1911',
   wky: '1912',
   wad: '1913',
   cad: '1914',
-  bd: '1915'
-}
+  bd: '1915',
+};
 
 export const searchBy = {
   name: '1',
@@ -57,35 +63,35 @@ export const displaySize = {
   pp: 2122,
   bt: 2123,
   sp: 2124,
-}
+};
 
 export const addressType = {
   main: 1,
   bill: 2,
-}
+};
 
 // TODO: change to number
 export const roleType = {
-  staff: '0',     // 2197
-  director: '1',  // 2198
-  manager: '2',   // 2199
-  admin: '3',     // 2200
-}
+  staff: '0', // 2197
+  director: '1', // 2198
+  manager: '2', // 2199
+  admin: '3', // 2200
+};
 
 // TODO: change to number
 export const permissionType = {
-  read: '0',  // 23
+  read: '0', // 23
   write: '1', // 24
-  delete: '2',  // 25
-}
+  delete: '2', // 25
+};
 
 export const deptType = {
   director: 'director', //18,
   sales: 'sales', //19,
   sub: 'sub', // 20,
   account: 'account', //21,
-  it: 'it' //22,
-}
+  it: 'it', //22,
+};
 
 export const reissueType = {
   reissue: 1,
--- src/domain/Collection/index.js
+++ src/domain/Collection/index.js
@@ -1,37 +1,96 @@
-import React, { useCallback, useEffect, useState, useRef } from "react";
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Grid, LinearProgress } from "@mui/material";
-import { useFormik } from "formik";
+import { Box, Grid, LinearProgress } from '@mui/material';
+import { useFormik } from 'formik';
 import { AgGridReact } from 'ag-grid-react';
 import { formatUIDate, numberWithCommas, precisionRound } from 'shared/utils';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import 'assets/styles/table.css';
 import * as Yup from 'yup';
-import useInvoice from "shared/hooks/useInvoice";
+import useInvoice from 'shared/hooks/useInvoice';
 import { useUserAuth } from 'shared/contexts/UserAuthContext';
 import { deptType } from 'data/constants';
-import BillList from "./BillList";
+import BillList from './BillList';
 
 const Collection = () => {
   const gridRef = useRef();
   const [paymentBills, setPaymentBills] = useState(null);
   const [selectedNode, setSelectedNode] = useState(null);
   const { isLoading, invoices, fetchInvoices, saveBills } = useInvoice(true);
-  const { user: { dept } } = useUserAuth();
+  const {
+    user: { dept },
+  } = useUserAuth();
   const navigate = useNavigate();
 
   const columnDefs = [
-    { field: 'invoiceNo', headerName: 'Invoice No', width: 220, },
+    { field: 'invoiceNo', headerName: 'Invoice No', width: 220 },
     { field: 'primaryName', headerName: 'Company', minWidth: 300 },
-    { field: 'dueDate', headerName: 'Due Date', width: 130, valueFormatter: (params) => formatUIDate(params.value) },
-    { field: 'issuedDate', headerName: 'Issued Date', width: 150, valueFormatter: (params) => formatUIDate(params.value) },
-    { field: 'cost', headerName: 'Cost', width: 120, editable: true, resizable: false, valueFormatter: (params) => numberWithCommas(precisionRound(params.value)) },
-    { field: 'tax', headerName: 'Tax', width: 100, editable: true, resizable: false, valueFormatter: (params) => numberWithCommas(precisionRound(params.value)) },
-    { field: 'orgTotal', headerName: 'Total', width: 100, editable: true, resizable: false, valueFormatter: (params) => numberWithCommas(precisionRound(params.value)) },
-    { field: 'outstandingCost', headerName: 'Outstanding Cost', width: 180, editable: true, resizable: false, valueFormatter: (params) => numberWithCommas(precisionRound(params.value)) },
-    { field: 'outstandingTax', headerName: 'Outstanding Tax', width: 170, editable: true, resizable: false, valueFormatter: (params) => numberWithCommas(precisionRound(params.value)) },
-    { field: 'outstandingTotal', headerName: 'Outstanding Total', width: 170, resizable: false, valueFormatter: (params) => numberWithCommas(precisionRound(params.value)) },
+    {
+      field: 'dueDate',
+      headerName: 'Due Date',
+      width: 130,
+      valueFormatter: (params) => formatUIDate(params.value),
+    },
+    {
+      field: 'issuedDate',
+      headerName: 'Issued Date',
+      width: 150,
+      valueFormatter: (params) => formatUIDate(params.value),
+    },
+    {
+      field: 'cost',
+      headerName: 'Cost',
+      width: 120,
+      editable: true,
+      resizable: false,
+      valueFormatter: (params) =>
+        numberWithCommas(precisionRound(params.value)),
+    },
+    {
+      field: 'tax',
+      headerName: 'Tax',
+      width: 100,
+      editable: true,
+      resizable: false,
+      valueFormatter: (params) =>
+        numberWithCommas(precisionRound(params.value)),
+    },
+    {
+      field: 'orgTotal',
+      headerName: 'Total',
+      width: 100,
+      editable: true,
+      resizable: false,
+      valueFormatter: (params) =>
+        numberWithCommas(precisionRound(params.value)),
+    },
+    {
+      field: 'outstandingCost',
+      headerName: 'Outstanding Cost',
+      width: 180,
+      editable: true,
+      resizable: false,
+      valueFormatter: (params) =>
+        numberWithCommas(precisionRound(params.value)),
+    },
+    {
+      field: 'outstandingTax',
+      headerName: 'Outstanding Tax',
+      width: 170,
+      editable: true,
+      resizable: false,
+      valueFormatter: (params) =>
+        numberWithCommas(precisionRound(params.value)),
+    },
+    {
+      field: 'outstandingTotal',
+      headerName: 'Outstanding Total',
+      width: 170,
+      resizable: false,
+      valueFormatter: (params) =>
+        numberWithCommas(precisionRound(params.value)),
+    },
   ];
 
   const onSelectionChanged = useCallback(() => {
@@ -41,36 +100,48 @@ const Collection = () => {
       setSelectedNode(selectedNodes[0]);
       setPaymentBills(selectedNodes[0].data.paymentBills);
     }
-  }, [])
+  }, []);
 
   const onSave = (paidBills) => {
     // const selectedNode = gridRef.current.api.getSelectedNodes()[0];
     setPaymentBills(null);
     fetchInvoices();
   };
-  
+
   useEffect(() => {
-    if (paymentBills) fmk.resetForm({ values: { bills: paymentBills }});
+    if (paymentBills) fmk.setValues({ bills: paymentBills });
   }, [paymentBills]);
 
   let fmk = useFormik({
     validationSchema: Yup.object().shape({
       bills: Yup.array(
         Yup.object({
-          paidCost: Yup.string().required("paidCost required"),
-          paidTax: Yup.string().required("paidTax required"),
-          paidDate: Yup.string().required("paidDate required"),
+          paidCost: Yup.string().required('paidCost required'),
+          paidTax: Yup.string().required('paidTax required'),
+          paidDate: Yup.string().required('paidDate required'),
         })
-      )
+      ),
     }),
     initialValues: { bills: paymentBills },
-    initialErrors: { bills: [{ paidCost: 'paidCost required', paidTax: 'paidTax required', paidDate: 'paidDate required', method: 'method reqired' }] },
+    initialErrors: {
+      bills: [
+        {
+          paidCost: 'paidCost required',
+          paidTax: 'paidTax required',
+          paidDate: 'paidDate required',
+          method: 'method reqired',
+        },
+      ],
+    },
     onSubmit: (values) => saveBills(values, selectedNode.id, onSave),
-    enableReinitialize: true,
+    enableReinitialize: true, //rerenders when initialvalues change
     validateOnBlur: true,
   });
 
-  if (dept.toLowerCase() === deptType.sales || dept.toLowerCase() === deptType.sub) {
+  if (
+    dept.toLowerCase() === deptType.sales ||
+    dept.toLowerCase() === deptType.sub
+  ) {
     navigate('/s/dashboard');
     return null;
   }
@@ -78,7 +149,12 @@ const Collection = () => {
   return (
     <>
       {isLoading && (
-        <Grid container justifyContent="center" alignItems="center" sx={{ height: '80vh', margin: "0 auto" }}>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          sx={{ height: '80vh', margin: '0 auto' }}
+        >
           <Box sx={{ minHeight: 4, width: 500 }}>
             <LinearProgress />
           </Box>
@@ -88,7 +164,10 @@ const Collection = () => {
       {!isLoading && (
         <Grid container direction="column" rowGap={5}>
           <Grid item sx={{ mb: 5 }}>
-            <Box sx={{ width: '100%', height: 600 }} className="ag-theme-alpine">
+            <Box
+              sx={{ width: '100%', height: 600 }}
+              className="ag-theme-alpine"
+            >
               <AgGridReact
                 ref={gridRef}
                 rowData={invoices}
@@ -98,21 +177,22 @@ const Collection = () => {
                   resizable: true,
                   filter: true,
                 }}
-                getRowId={params => params.data.invoiceNo}
+                getRowId={(params) => params.data.invoiceNo}
                 rowSelection="single"
                 onSelectionChanged={onSelectionChanged}
                 pagination={true}
                 paginationPageSize={20}
-              >
-              </AgGridReact>
+              ></AgGridReact>
             </Box>
           </Grid>
 
-          {fmk.values.bills && paymentBills && <BillList {...{ fmk, isLoading }} />}
+          {fmk.values.bills && paymentBills && (
+            <BillList {...{ fmk, isLoading }} />
+          )}
         </Grid>
       )}
     </>
   );
-}
+};
 
 export default Collection;
